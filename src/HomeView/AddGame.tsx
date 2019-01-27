21dab6d0b01f0ea8c996fc0126e6159fc280e853
sd <sdebaun74@gmail.com>
2019-01-27 13:21:55 -0800

    create game mutation

---
+++ src/HomeView/AddGame.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Button, Grow } from '@material-ui/core';
+import { Formik } from 'formik';
+import gql from 'graphql-tag';
+import { useMutation } from 'react-apollo-hooks';
+
+type FormikValues = {
+  bigPicture: string
+}
+
+const CREATE_GAME = gql`
+  mutation CreateGame(
+    $bigPicture: String!
+  ) {
+    createGame(
+      ownerId: "cjrfcd115amje01169ghfbrmp"
+      bigPicture: $bigPicture
+    ) {
+      id
+    }
+  }
+`
+
+const AddGameForm: React.SFC = () => {
+  const createGame = useMutation(CREATE_GAME)
+
+  return (
+    <Formik<FormikValues>
+      initialValues={{bigPicture: ''}}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log('values', values)
+        createGame({variables: values})
+        setSubmitting(false)
+      }}
+      >
+      {({values, handleChange, handleBlur, handleSubmit}) => (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='bigPicture'>
+            Big Picture
+          </label>
+          <input id='bigPicture' type='text' placeholder='Whats the big picture?'
+            value={values.bigPicture}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            />
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+
+export const AddGame: React.SFC = () => {
+  const [adding, setAdding] = useState(false)
+
+  if (adding) { return (
+    <Grow><>
+        <AddGameForm />
+        <Button variant='contained' onClick={() => setAdding(false)}>Cancel</Button>
+    </></Grow>
+  )}
+  return (
+    <div>
+      <Button variant='contained' color='primary' onClick={() => setAdding(true)}>Add Game</Button>
+    </div>
+  )
+}
--- src/HomeView/User.tsx
+++ src/HomeView/User.tsx
@@ -4,6 +4,7 @@ import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo-hooks'
 import { CircularProgress } from '@material-ui/core'
 import { AllGames, AllGames_allGames } from './types/AllGames'
+import { AddGame } from './AddGame';
 
 const ALL_GAMES = gql`
   query AllGames {
@@ -20,6 +21,7 @@ const GameLink: React.SFC<{game: AllGames_allGames}> = ({game: {id, bigPicture}}
 const GameList: React.SFC = () => {
   const { data, error } = useQuery<AllGames>(ALL_GAMES)
   if ( error || !data ) { return <div>Error!</div>}
+  if (data.allGames.length == 0) { return <div>You have no games!</div>}
   return <>
     {data.allGames.map((game) => <GameLink {...{key: game.id, game}}/>)}
   </>
@@ -30,5 +32,6 @@ export const User: React.SFC<{className?: string}> = ({className}) =>
     <h1>The User</h1>
     <Suspense fallback={<CircularProgress color='secondary'/>}>
       <GameList/>
+      <AddGame/>
     </Suspense>
   </div>
