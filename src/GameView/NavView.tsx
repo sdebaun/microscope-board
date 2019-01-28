7d4a7cbe7f67f5678ae03446fe00021ea6d40c2f
sd <sdebaun74@gmail.com>
2019-01-28 12:30:29 -0800

    cleanup

--- src/GameView/NavView.tsx
+++ src/GameView/NavView.tsx
@@ -9,9 +9,3 @@ export const NavView: React.SFC<{game: GetGame_Game}> = ({game: {id}}) =>
     <Link to='/'><ArrowBackIosIcon/></Link>
     <div><MenuIcon/></div>
   </>
-
-// export const NavView: React.SFC<{game: GetGame_Game, className?: string}> = ({game: {id}, className}) =>
-//   <div {...{className}}>
-//     <Link to='/'><ArrowBackIosIcon/></Link>
-//     <div><MenuIcon/></div>
-//   </div>
